7da171d7ec3922c3e717b4116212bf9526087ad7
Samantha Alvarenga <samantha.c.alvarenga@gmail.com>
2025-05-08 18:02:08 -0300

    Adicionado telas 'Android' e 'IOS' no Stack.Navigator

--- App.js
+++ App.js
@@ -1,29 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+
+import HomeScreen from './HomeScreen';
+import Android from './pages/Android';
+import Ios from './pages/Ios';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-   /*  <ImageBackground source={{
-      uri:
-        'https://img.freepik.com/fotos-gratis/plano-de-fundo-de-programacao-com-pessoa-trabalhando-com-codigos-no-computador_23-2150010125.jpg?t=st=1746665755~exp=1746669355~hmac=ac7dee82b65a655caca19e7665b73f47c0c418e78fbec842cbf35259f6b10557&w=996'
-    }} > */
-      <View style={styles.container}>
-        <Text style={styles.title}> Sistemas{'\n'} Operacionais </Text>
-        <StatusBar style="auto" />
-      </View>
-      
+
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name = "Android" component = {Android}/>
+         <Stack.Screen name = "IOS" component = {Ios}/>  
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
+
+
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title:{
-    fontSize: 25,
-    fontWeight: 'bold'
-  }
-});
